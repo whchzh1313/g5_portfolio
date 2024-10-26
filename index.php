60c27935e4876e6edc59089be6563a2fac678d7d
whchzh1313 <79441700+whchzh1313@users.noreply.github.com>
2024-10-26 13:28:39 +0000

    feat: add aos

--- index.php
+++ index.php
@@ -18,13 +18,13 @@ include_once(G5_PATH.'/head.php');
 ?>
     <div id="main">
         <div id="titleSection" class="blue_gradiant_bg center_y_title">
-            <div class="section wave_text">
+            <div class="section wave_text" data-aos="fade-up">
                 <p class="words wave" aria-hidden="true">이화민<br />웹 개발자 포트폴리오</p>
                 <p class="words">이화민<br />웹 개발자 포트폴리오</p>
             </div>
         </div>
         <div id="aboutMe" class="section">
-            <h2 class="title_circle blue_circle"><span>About me</span></h2>
+            <h2 class="title_circle blue_circle"><span data-aos="fade-up">About me</span></h2>
             <div class="flex_box profile_wrap">
                 <div class="flex_l">
                     <div id="ProfileImageContainer">
@@ -32,19 +32,19 @@ include_once(G5_PATH.'/head.php');
                     </div>
                 </div>
                 <div class="flex_r">
-                    <div class="flex_box">
+                    <div class="flex_box" data-aos="fade-up">
                         <h3>이화민</h3>
                         <p>1997.07.15</p>
                     </div>
-                    <p class="font_gray">Web Developer</p>
-                    <p>새로운 환경과 변화에 빠르게 적응하며, <br />
+                    <p class="font_gray" data-aos="fade-up">Web Developer</p>
+                    <p data-aos="fade-up">새로운 환경과 변화에 빠르게 적응하며, <br />
                     끊임없이 성장하고 도전을 즐기는 웹 개발자입니다.</p>
                 </div>
             </div>
             <div id="skill" class="inner_section">
-                <h3>Skill</h3>
+                <h3 data-aos="fade-up">Skill</h3>
                 <div id="skillWrap" class="flex_box flex_wrap">
-                    <div class="skills web">
+                    <div class="skills web" data-aos="fade-up" data-aos-delay="0">
                         <h4>Web</h4>
                         <ul class="flex_box skill_chips">
                             <li class="chip radius_100">HTML</li>
@@ -54,7 +54,7 @@ include_once(G5_PATH.'/head.php');
                             <li class="chip radius_100">Gnuboard</li>
                         </ul>
                     </div>
-                    <div class="skills design">
+                    <div class="skills design" data-aos="fade-up" data-aos-delay="125">
                         <h4>Design & Prototyping</h4>
                         <ul class="flex_box skill_chips">
                             <li class="chip radius_100">Figma</li>
@@ -62,21 +62,21 @@ include_once(G5_PATH.'/head.php');
                             <li class="chip radius_100">Illustrator</li>
                         </ul>
                     </div>
-                    <div class="skills tool">
+                    <div class="skills tool" data-aos="fade-up" data-aos-delay="250">
                         <h4>Tool</h4>
                         <ul class="flex_box skill_chips">
                             <li class="chip radius_100">VSCode</li>
                             <li class="chip radius_100">Android Studio</li>
                         </ul>
                     </div>
-                    <div class="skills version">
+                    <div class="skills version" data-aos="fade-up" data-aos-delay="375">
                         <h4>Version Control & Collaboration</h4>
                         <ul class="flex_box skill_chips">
                             <li class="chip radius_100">Notion</li>
                             <li class="chip radius_100">Slack</li>
                         </ul>
                     </div>
-                    <div class="skills mobile_app">
+                    <div class="skills mobile_app" data-aos="fade-up" data-aos-delay="500">
                         <h4>MobileApp</h4>
                         <ul class="flex_box skill_chips">
                             <li class="chip radius_100">Kotlin</li>
@@ -88,7 +88,7 @@ include_once(G5_PATH.'/head.php');
         </div>
         <div id="project" class="blue_bg">
             <div class="section">
-                <h2 class="title_circle white_circle"><span>Project</span></h2>
+                <h2 class="title_circle white_circle"><span data-aos="fade-up">Project</span></h2>
             </div>
         </div>
     </div>
